b9247ec38a322c3d599465b9caa8dc9a3617b9e0
Vaibhav Salve <144205049+abcdjjejejsj@users.noreply.github.com>
2025-10-27 15:57:53 +0530

    Update feed.js

--- Routes/feed.js
+++ Routes/feed.js
@@ -2,8 +2,32 @@ const category=require("../Model/feedback");
 const express=require('express');
 const handler=require("../Controllers/feedHandler");
 const router=express.Router();
+const jwt = require('jsonwebtoken');
+const sec = process.env.secret_key;
 
-router.post("/addFeed",handler.addFeed);
+function validateUser(req, res, next) {
+    const token = req.cookies.token;
+    if (!token) return res.redirect("https://shubhangi-collection.vercel.app/register.html");
+
+    jwt.verify(token, sec, (err, user) => {
+        if (err) {
+            res.clearCookie("token", {
+               httpOnly: true,
+  secure: true,         // ✅ because both backend & frontend are HTTPS
+  sameSite: "none", 
+            });
+
+            return res.sendStatus(403);
+        };
+        req.user = user; // decoded payload
+        console.log("data from token:", user);
+        console.log("user.email :", user.email);
+        console.log("password :",user.password)
+        next();
+    });
+}
+
+router.post("/addFeed",validateUser,handler.addFeed);
 
 router.get("/sendFeed",handler.sendFeed);
 
